ef29e85b638ef2a9d1fe6bd229068d413aa40a42
mincong <hemincong@gmail.com>
2023-04-14 22:34:53 +0800

    fix softmax

--- common/functions.py
+++ common/functions.py
@@ -5,9 +5,16 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def softmax(a):
-    exp_a = np.exp(a)
-    return exp_a / np.sum(exp_a)
+def softmax(x):
+    # 和P65不一样
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+
+    x = x - np.max(x) # 溢出对策
+    return np.exp(x) / np.sum(np.exp(x))
 
 
 def cross_entropy_error(y, t):
