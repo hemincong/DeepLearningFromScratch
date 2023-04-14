5fb806f19d635915dd12b0286d6f55bcb66ede94
mincong <hemincong@gmail.com>
2023-04-14 22:33:07 +0800

    two layer net + gradient

--- two_layer_net.py
+++ two_layer_net.py
@@ -4,7 +4,8 @@ from common.functions import *
 from common.gradient import numerical_gradient
 
 sys.path.append(os.pardir)
-
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
 
 class TwoLayerNet:
 
@@ -12,8 +13,8 @@ class TwoLayerNet:
         self.params = {
             'W1': weight_init_std * np.random.randn(input_size, hidden_size),
             'b1': np.zeros(hidden_size),
-            'W2': weight_init_std * np.random.randn(input_size, hidden_size),
-            'b2': np.zeros(hidden_size)
+            'W2': weight_init_std * np.random.randn(hidden_size, output_size),
+            'b2': np.zeros(output_size)
         }
 
     def predict(self, x):
@@ -46,4 +47,29 @@ class TwoLayerNet:
             'W2': numerical_gradient(loss_W, self.params['W2']),
             'b2': numerical_gradient(loss_W, self.params['b2']),
         }
+        return grads
+
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
+
         return grads
